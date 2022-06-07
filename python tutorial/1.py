2dc3ddddaddd8cc41544c583cb469377fce80ef8
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-06-07 13:18:33 -0500

    Add files via upload

---
+++ python tutorial/1.py
@@ -0,0 +1,65 @@
+# print("This is a string")
+
+# whatever after it 
+
+"""
+whatever
+you put inside of this
+"""
+"""
+x = 4
+x = "bob"
+
+print(x)
+"""
+"""
+x = str(3) # '3'
+y = float(3) # 3.0
+w = int(3) # 3
+
+print(x)
+print(y)
+print(w)
+
+# type()
+
+print(type(x))
+print(type(w))
+print(type(y))
+"""
+'' """
+x = 1
+X = 3
+
+print(X)
+"""
+#Variable names MUST start with a letter or an underscore, variables cannot start with a number. only can contain alpha numeric A-z 0-9 and underscore 
+
+"""
+#Good 
+x = 1
+X = 3
+_x = 17
+Var = 6
+var2 = 7
+
+#bad
+#2var = 11
+
+
+print(x, X, _x, Var, var2)
+"""
+"""
+#Cases
+# Camel Case
+myVar = 1
+
+#Pascal Case
+MyVar = 2
+
+#Snake Case
+my_var = 3
+
+print(myVar, MyVar, my_var)
+
+"""
