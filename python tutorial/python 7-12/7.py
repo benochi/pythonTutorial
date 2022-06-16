3459e730cf493705e119fb26cd4b29ebe48165c0
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-06-16 14:40:27 -0500

    Add files via upload

---
+++ python tutorial/python 7-12/7.py
@@ -0,0 +1,51 @@
+"""
+print(1 < 3)
+print(8 == 7)
+print(3 < 1)
+"""
+"""
+x = 10
+y = 50
+
+if x > y:
+  print("x is greater than y.")
+  print("I ran under the if statement.")
+else:
+  print("x is not greater than y.")
+  print("I ran under the else statement.")
+  """
+
+#print(bool())
+
+"""
+x = "string"
+y = 0
+
+print(bool(x))
+print(bool(y))
+"""
+
+#print(bool(["apple", "banana"]))
+
+"""
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+"""
+"""
+def myFunc():
+  return True
+
+#print(myFunc())
+
+if myFunc():
+  print("YES!")
+else:
+  print("NO!")
+"""
+
+x = 10
+print(isinstance(x, int))
