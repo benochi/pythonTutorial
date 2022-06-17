b8f38491564621f7dec6c36594e50a59e5050ad9
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-06-17 10:11:37 -0500

    Add files via upload

---
+++ python tutorial/python 7-12/8.py
@@ -0,0 +1,63 @@
+"""
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
+print(5 % 2)
+print(5 ** 3)
+print(5 // 2)
+"""
+
+
+"""
+x += 2 
+x -= 2
+x *= 2
+x /= 2
+x %= 2
+"""
+#x //= 3
+#x **= 2
+#x &= 3 
+#x |= 3
+#x ^= 3
+# >>=
+# <<=
+"""
+x = 10
+y = 8
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x <= y)
+print(x >= y)
+"""
+"""
+x = 8
+
+print(x < 5 and x < 10)
+print(x < 5 or x < 10)
+print(not(x > 5 and x < 10))
+"""
+"""
+x = 8
+y = 8
+
+print(x is y)
+print(x is not y)
+"""
+"""
+x = ["one", "two"]
+
+print("one" in x)
+print("two" not in x)
+"""
+
+#  & and
+#  | OR
+#  ^ XOR 
+#  ~ NOT 
+#  << Zero fill left shift
+#  >> signed right shift
+
