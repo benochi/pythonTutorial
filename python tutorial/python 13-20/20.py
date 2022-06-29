11fd3b2936b9c7d5839a809af80b2139cf663c14
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-06-29 08:57:21 -0500

    Add files via upload

---
+++ python tutorial/python 13-20/20.py
@@ -0,0 +1,60 @@
+"""
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+"""
+
+#x = 10
+#y = 10
+
+#print("yes") if y > x else print("no")
+
+"""
+if x > y:
+  print("x is greater than y.")
+#elif x == y:
+#  print("x equals y")
+else:
+  print("y is greater than x.")
+"""
+
+
+
+"""
+print("1") if x > y else print("2") if x == y else print("3")
+
+if x > y:
+  print("1")
+elif x ==y:
+  print("2")
+else:
+  print("3")
+"""
+"""
+x = 10
+y = 9
+z = 800
+#and/ or
+if x < y or z < x:
+  print("at least one is true")
+else:
+  print("Both are not true")
+"""
+
+#x = 1
+"""
+if x > 10:
+  print("x is greater than 10.")
+  if x > 20:
+    print("x is also greater than 20")
+  else:
+    print("x is not greater than 20.")
+"""
+x = 10
+y = 9
+
+if x > y:
+  pass
