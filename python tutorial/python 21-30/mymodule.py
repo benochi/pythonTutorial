fa89ef06d6370f1f5b92a294857b299084bbf8f6
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-07-20 16:35:44 -0500

    Add files via upload

---
+++ python tutorial/python 21-30/mymodule.py
@@ -0,0 +1,11 @@
+def myfunc(string):
+  print("this is inside my module. " + string)
+
+def myfunc2(string):
+  print("this is inside my module. " + string)
+
+
+dog1 = {
+  "name": "dog1",
+  "age": 10
+}
