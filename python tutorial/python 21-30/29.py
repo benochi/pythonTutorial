f263dcff639c292e51fcac65bb6ca2b6bd0e5fe7
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-07-20 16:35:34 -0500

    Add files via upload

---
+++ python tutorial/python 21-30/29.py
@@ -0,0 +1,22 @@
+#import mymodule as mm
+import platform
+
+from mymodule import dog1
+
+#y = platform.system()
+#print(y)
+
+"""
+mm.myfunc("string")
+
+mm.myfunc2("bob")
+
+x = mm.dog1["name"]
+print(x)
+"""
+
+#x = dir(mm)
+#print(x)
+
+#print(dog1["name"])
+# do not do this if using from - print(mymodule.dog1["name"])
