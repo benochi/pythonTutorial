852d0136e2727b769cb8a1d2b7297cc2b35fc21b
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-07-20 17:52:54 -0500

    Add files via upload

---
+++ python tutorial/python 31-40/31.py
@@ -0,0 +1,29 @@
+"""
+x = min(1, 5, 10)
+y = max(1, 5, 10)
+
+#print(x)
+#print(y)
+
+w = abs(-8.56)
+
+print(w)
+"""
+
+#x = pow(5, 6)
+
+#print(x)
+
+import math 
+
+#x = math.sqrt(71)
+
+#x = math.ceil(1.4)
+#y = math.floor(1.4)
+
+x = math.pi
+
+print(x)
+#print(y)
+
+print(math.nan)
