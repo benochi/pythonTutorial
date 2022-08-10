c4cecf5d4945283d923e93c479595e7f1f106dcf
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-08-10 08:00:03 -0500

    Create introExercise.py

---
+++ python tutorial/exercises/introExercise.py
@@ -0,0 +1,123 @@
+#import the module you made AND rename it
+
+
+#assign the data objects that you built (Set, List, Tuple, Dict) from your module
+
+
+#print one of the unpacked variables from your module
+
+
+#add user input asking for a first and last name
+
+
+#use the split method to cut the string into first and last name
+
+#capitalize the first letter in each name 
+
+
+#return a string and concatonate using the + operator to say "Your first name is <firstName> and your last name is <lastName>" 
+#using the users inputed names in place of the placeholders in my example. 
+
+
+#add user input in the CLI(command line interface) and assign it to a variable
+
+
+#build a if/elif/else to handle user input and check the user input string equality with a comparison operator(==)
+#if user input does NOT equal the options given, pass
+
+  #print an item from the tuple by index
+
+
+  #print the set, remember how indexing and slicing don't work in sets like other objects. 
+  
+  #print an item from the list by index
+ 
+  #Extra credit - check the dict item to see if it returns a True or False boolean value and modify output accordingly. 
+  #In the case of my example, it would be checking if the item is 'inStock' 
+  #If you want practice you can play around with the 'and' operator, but for my example a nested if(): makes more sense. 
+
+    #print an item from the dict by index and return the name value by using the key 'name'
+    
+
+#make a list of imaginary clients and
+
+
+#build a method that loops over that list, printing each of them.
+
+
+#execute that method Be sure to pass in any necessary arguments.
+#The proper scope should have the list outside of the method. 
+
+
+#write a lambda method that multiplies an arg by .07%
+
+
+#assign the price of an item in your dict to a variable
+
+#use the lambda function to find the tax of the item price. 
+
+
+#add the tax and the original price for the total price and print that total
+#Extra credit - Use format to make sure only 2 decimals are returned
+
+#Note - think about using ceil / floor to round change if needed. Remember to import Math if you try this.
+
+#build a while loop that pops customers from the list, prints the customer and stops when the list is empty. 
+#be careful to make it stop if there are no customers and Ctrl+C is your friend. 
+
+
+#build a customer class
+#give customers an __init__ a name and an age.
+
+#make a customer
+
+
+#print the customers name and age
+
+
+#add a customer method to your class that says "My name is " and uses their name.
+#do this in the base class and call that function here. 
+
+
+#modify the customer age and print it
+
+
+#now make a child class that inherits from the Customer class
+#Remember you can pass to give it the same attributes only. 
+
+
+#use the child to create an object instance
+
+#use the parents function on the child
+
+
+
+#make an iterator for the tuple, in my example it is 'free'
+#Lists, Tuples, dictionaries, and sets are all iterable in this fashion
+
+
+#print a few values using the iterator and keyword next
+
+
+#practice scope 
+#Make a method that has a global variable inside of it and assigns a value to that variable
+
+
+#call the method
+
+
+#print the global variable outside of the method
+
+
+#import the regex module at the top it is called 're'
+#make a variable with a string value
+
+
+
+#use regex to search for matching words in the string
+
+#print the results, if you are getting "None", check your work.
+
+
+#build a try/except/finally block and have it return an error
+#this is the last thing our application does, so feel free to play around with it. 
